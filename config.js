257e3eeac9393b1f5622a8ec73403869b80c4749
wille5656 <william.edwards@cix.csi.cuny.edu>
2021-01-21 18:21:34 -0500

    CRUD

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://classed:CzqHywq9fNV2F8vz@cluster0.pyfrc.mongodb.net/merng?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
+
+
+
+
+//CzqHywq9fNV2F8vz
